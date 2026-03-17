dbb4cbdd7310e799a817f1c0cfaa5a0cf836e1a3
Daniel Mania <daniel.mania@radiozeit.de>
2026-03-17 11:44:02 +0100

    feat: deep link support for Android via platform channel

--- android/app/src/main/kotlin/de/radiozeit/freiesradio/MainActivity.kt
+++ android/app/src/main/kotlin/de/radiozeit/freiesradio/MainActivity.kt
@@ -1,6 +1,42 @@
 package de.radiozeit.freiesradio
-import com.ryanheise.audioservice.AudioServiceActivity;
-import io.flutter.embedding.android.FlutterActivity
+
+import android.content.Intent
+import com.ryanheise.audioservice.AudioServiceActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.EventChannel
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: AudioServiceActivity() {
+    private val CHANNEL = "de.radiozeit.freiesradio/deeplink"
+    private val EVENT_CHANNEL = "de.radiozeit.freiesradio/deeplink_events"
+    private var eventSink: EventChannel.EventSink? = null
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "getInitialLink") {
+                result.success(intent?.data?.toString())
+            } else {
+                result.notImplemented()
+            }
+        }
+        EventChannel(flutterEngine.dartExecutor.binaryMessenger, EVENT_CHANNEL).setStreamHandler(
+            object : EventChannel.StreamHandler {
+                override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
+                    eventSink = events
+                }
+                override fun onCancel(arguments: Any?) {
+                    eventSink = null
+                }
+            }
+        )
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        intent.data?.toString()?.let { uri ->
+            eventSink?.success(uri)
+        }
+    }
 }
